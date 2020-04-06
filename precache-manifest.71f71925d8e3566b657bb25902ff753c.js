c5542f7e9df536cb471582907add8c8ce5cbf07a
djohal <dilpreet.johal022@gmail.com>
2020-04-05 20:03:15 -0400

    Updates

---
+++ precache-manifest.71f71925d8e3566b657bb25902ff753c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdef6456271a60e21cf76e3594b8942",
+    "url": "/image-gallery-react/index.html"
+  },
+  {
+    "revision": "c91235b4d3f7acdf2584",
+    "url": "/image-gallery-react/static/css/main.c22c3e92.chunk.css"
+  },
+  {
+    "revision": "7aeb2e615187d2ebc1fa",
+    "url": "/image-gallery-react/static/js/2.aa1085b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-gallery-react/static/js/2.aa1085b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91235b4d3f7acdf2584",
+    "url": "/image-gallery-react/static/js/main.b51f4e7b.chunk.js"
+  },
+  {
+    "revision": "5307abc10b61ef87ada4",
+    "url": "/image-gallery-react/static/js/runtime-main.68d8335e.js"
+  }
+]);
